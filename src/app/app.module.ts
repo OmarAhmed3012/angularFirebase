7634a164e9816d328f03231205e5f5f13f48bd19
OmarAhmed3012 <omaraboshamaa@gmail.com>
2020-04-14 01:38:06 +0200

    some changes on the start

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { environment } from '../environments/environment';
 
 import { AppComponent } from './app.component';
+import { ItemsComponent } from './components/items/items.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ItemsComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+  	AngularFirestoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,17 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebaseConfig : {
+    apiKey: "AIzaSyCU6Yc4J_xcjqVxRjM6IyWdMcuXna1wsKU",
+    authDomain: "fs1prod-191fa.firebaseapp.com",
+    databaseURL: "https://fs1prod-191fa.firebaseio.com",
+    projectId: "fs1prod-191fa",
+    storageBucket: "fs1prod-191fa.appspot.com",
+    messagingSenderId: "500971985096",
+    appId: "1:500971985096:web:19c95b0875a703c9dfadfd",
+    measurementId: "G-4JEZ508ES0"
+  }
 };
 
 /*
