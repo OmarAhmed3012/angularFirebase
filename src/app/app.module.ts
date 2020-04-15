236d6eb4d93782e48f59a6c51d05baf6cd75f556
OmarAhmed3012 <omaraboshamaa@gmail.com>
2020-04-16 00:46:32 +0200

    add Items

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { environment } from '../environments/environment';
 import { AngularFireModule } from 'angularfire2';
 import { AngularFirestoreModule } from 'angularfire2/firestore';
 
+import { FormsModule } from '@angular/forms'
+
 /*import { AngularFireModule } from '@angular/fire';
 import { AngularFirestore } from 'angularfire2/firestore'
 import { AngularFirestoreModule } from '@angular/fire/firestore';
@@ -15,16 +17,21 @@ import { ItemsComponent } from './components/items/items.component';
 
 
 import { ItemService } from './services/item.service';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { AddItemComponent } from './components/add-item/add-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ItemsComponent,
+    NavbarComponent,
+    AddItemComponent,
   ],
   imports: [
     BrowserModule,
     AngularFireModule.initializeApp(environment.firebaseConfig, 'angularfs'),
-    AngularFirestoreModule
+    AngularFirestoreModule,
+    FormsModule
     /*
     AngularFireModule.initializeApp(environment.firebaseConfig),
   	AngularFirestoreModule,*/
--- src/app/services/item.service.ts
+++ src/app/services/item.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection, AngularFirestoreDocument } from 'angularfire2/firestore'
 import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators'
 
 
 import {Item} from '../models/item'
@@ -13,14 +14,35 @@ import {Item} from '../models/item'
 export class ItemService {
   itemsCollection: AngularFirestoreCollection<Item>;
   items: Observable<Item[]>;
-
+  itemDoc: AngularFirestoreDocument<Item>
+ 
   constructor(public afs: AngularFirestore) {
-    this.items = this.afs.collection('items').valueChanges();
+    //this.items = this.afs.collection('items').valueChanges();
+
+    this.itemsCollection = this.afs.collection('items', ref => ref.orderBy('title','asc'));
+
+    this.items = this.itemsCollection.snapshotChanges().pipe( map(changes => {
+      return changes.map(a => {
+        const data = a.payload.doc.data() as Item;
+        data.id = a.payload.doc.id;
+        return data;
+      })
+    }))
    }
 
    getItems() {
      return this.items;
    }
+
+   addItem(item: Item){
+     this.itemsCollection.add(item);
+   }
+
+   deleteItem(item: Item) {
+     this.itemDoc = this.afs.doc(`items/${item.id}`);
+     this.itemDoc.delete();
+   }
+
 }
 
 
