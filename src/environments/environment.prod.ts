58fe1e5b4299c1ebe952faa69cdf635a467bba31
OmarAhmed3012 <omaraboshamaa@gmail.com>
2020-08-23 14:14:33 +0200

    ..

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebaseConfig : {
+    apiKey: "AIzaSyCU6Yc4J_xcjqVxRjM6IyWdMcuXna1wsKU",
+    authDomain: "fs1prod-191fa.firebaseapp.com",
+    databaseURL: "https://fs1prod-191fa.firebaseio.com",
+    projectId: "fs1prod-191fa",
+    storageBucket: "fs1prod-191fa.appspot.com",
+    messagingSenderId: "500971985096",
+    appId: "1:500971985096:web:19c95b0875a703c9dfadfd",
+    measurementId: "G-4JEZ508ES0"
+  }
 };
